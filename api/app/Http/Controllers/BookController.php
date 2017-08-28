8ecb5222606c6dbbe87994df87c3b00a9afa6c9d
Haoua <haouasou@gmail.com>
2017-08-28 11:41:07 +0200

    Nice display of book by title and book of author

--- api/app/Http/Controllers/BookController.php
+++ api/app/Http/Controllers/BookController.php
@@ -9,12 +9,57 @@ class BookController extends Controller{
 
     public function getBookByName($title){
         $data_book_url = file_get_contents('https://www.googleapis.com/books/v1/volumes?q=intitle:'.$title);
-        return response()->json( json_decode($data_book_url) );
+        $data_book = json_decode($data_book_url);
+
+        $books = array();
+
+
+        foreach ($data_book->items as $book) {
+            if ($book->volumeInfo->language == "fr") {
+
+                $author = "";
+                foreach ($book->volumeInfo->authors as $auth) {
+                    $author .= $auth." / ";
+                }
+
+                $author = substr($author, 0, -3);
+
+                $book_item = ["name" => $book->volumeInfo->title, 'author' => $author, 'image' => $book->volumeInfo->imageLinks->smallThumbnail, 'resume' => $book->volumeInfo->description, "published_year" => substr($book->volumeInfo->publishedDate, 0, 4)];
+
+                array_push($books, $book_item);
+            }
+        }
+            
+        return ['id' => '4-1', 'result' => $books];
+
+
+        // return response()->json( json_decode($data_book_url) );
     }
 
     public function getBookByAuthor($author){
         $data_book_url = file_get_contents('https://www.googleapis.com/books/v1/volumes?q=inauthor:'.$author);
-        return response()->json( json_decode($data_book_url) );
+        $data_book = json_decode($data_book_url);
+
+        $books = array();
+
+
+        foreach ($data_book->items as $book) {
+            if ($book->volumeInfo->language == "fr" && isset($book->volumeInfo->description)) {
+
+                $author = "";
+                foreach ($book->volumeInfo->authors as $auth) {
+                    $author .= $auth." / ";
+                }
+
+                $author = substr($author, 0, -3);
+
+                $book_item = ["name" => $book->volumeInfo->title, 'author' => $author, 'image' => $book->volumeInfo->imageLinks->smallThumbnail, 'resume' => $book->volumeInfo->description, "published_year" => substr($book->volumeInfo->publishedDate, 0, 4)];
+
+                array_push($books, $book_item);
+            }
+        }
+            
+        return ['id' => '4-2', 'result' => $books];
     }
 
     public function getBookByCategory($category){
